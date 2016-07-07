f090d3bf0d44aadbe3c8e3ea9e614ea30de9f9a7
Dru Abraham <D24Abraham@gmail.com>
2016-07-07 07:26:48 -0400

    PortBuild
    Added to github.io

---
+++ assets/function.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+// var int1 = setInterval(function(){
+//     var currentHour = new Date().getHours();
+//     var currentMin = new Date().getMinutes();
+//     var both = currentHour + currentMin
+
+	// if((both > 5) && (both < 12))
+		$('#cursing').typed({
+		strings:["Good morning and welcome to my", "Buenos días y bienvenidos a mi","Bom dia e boas-vindas ao meu","Guten Morgen und herzlich willkommen auf meiner", "早上好，欢迎来到我的", "おはようございますとへようこそ私", "Доброе утро и добро пожаловать в мой", "Tere hommikust ja tere tulemast minu"],
+		typeSpeed: 1,
+		backDelay: 3000,
+		loop: true,
+		loopCount: 500
+	});
+	// else if((both > 12) && (both < 16))
+	// 	$('#cursing').typed({
+	// 	strings:["GOOD AFTERNOON and welcome to my", "Buenos días y bienvenidos a mi","Bom dia e boas-vindas ao meu","Guten Morgen und herzlich willkommen auf meiner", "早上好，欢迎来到我的", "おはようございますとへようこそ私", "Доброе утро и добро пожаловать в мой", "Tere hommikust ja tere tulemast minu"],
+	// 	typeSpeed: 1,
+	// 	backDelay: 3000,
+	// 	loop: true,
+	// 	loopCount: 500
+
+	 });
+  // });
+// }
+
+
+	// var theTime = $.now()
+	// var scrollingText =
